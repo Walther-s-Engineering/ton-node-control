3b02521e6d162c65413ea33334743bfa82e72551
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2022-10-27 00:53:57 +0300

    Updated cli, added update method, added dependencies

--- manage_cli.py
+++ manage_cli.py
@@ -1,5 +1,16 @@
-import click
+import typing as t
 import subprocess
+import pkg_resources
+
+import click
+
+from subprocess import call
+
+from ton_node_control.cli.utils.messages import message
+from ton_node_control.utils.typing import String, Integer
+
+
+OPERATION_NOT_PERMITTED: t.Final[Integer] = 1
 
 main = click.Group()
 
@@ -11,8 +22,34 @@ def main() -> None:
 
 @main.command()
 def update():
-    print('Updating')
-
+    message(
+        'Updating "ton_node_control" and following packages:\n'
+        + '\n'.join(f'\t{package}' for package in pkg_resources.working_set),
+    )
+    must_proceed: bool = click.prompt(
+        type=bool,
+        text='Continue?',
+        prompt_suffix=' ',
+    )
+    if must_proceed is True:
+        for dist in pkg_resources.working_set:
+            package, _ = str(dist).split(' ')
+            package: String
+            exit_code: Integer = call(
+                'python -m pip install '
+                '--ignore-installed '
+                f'--upgrade {package}',
+                shell=True,
+            )
+            if exit_code == OPERATION_NOT_PERMITTED:
+                raise message(
+                    f'Failed to update package "{package}" '
+                    f'with "pip". Exit code: "{exit_code}"',
+                    exit_after=True,
+                )
+            raise message('Update successfully process completed.', exit_after=True)
+    raise message('Updated aborted.', exit_after=True)
+    
 
 cli = click.CommandCollection(sources=[main])
 
---
+++ ton_node_control/utils/typing.py
@@ -0,0 +1,4 @@
+import typing as t
+
+String = t.TypeVar('String', bound=str)
+Integer = t.TypeVar('Integer', bound=int)
