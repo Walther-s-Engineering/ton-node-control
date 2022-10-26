34d128f2a0ec63f875490fce8b3ae61c8f3722d4
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2022-10-26 22:23:10 +0300

    @WIP

--- manage_cli.py
+++ manage_cli.py
@@ -1,12 +1,20 @@
 import click
+import subprocess
 
 main = click.Group()
 
 
-@main.command()
-def application() -> None:
-    print('qqq')
+@click.group()
+def main() -> None:
+    pass
 
+
+@main.command()
+def update():
+    print('Updating')
+
+
+cli = click.CommandCollection(sources=[main])
 
 if __name__ == '__main__':
     main()
