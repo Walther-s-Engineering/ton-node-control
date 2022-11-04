ba2e3035a8f2b528e49fef0ba93e867fbb1f93c2
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2022-11-04 17:21:14 +0300

    Update installer. State: @WIP

--- installer.py
+++ installer.py
@@ -15,7 +15,9 @@ import subprocess
 import sys
 import re
 import tempfile
+import time
 import typing as t
+import tarfile
 import urllib.request
 
 from datetime import datetime
@@ -50,6 +52,23 @@ SHELL: String = os.getenv('SHELL', '')
 WINDOWS = sys.platform.startswith('win') or (sys.platform == 'cli' and os.name == 'nt')
 MACOS = sys.platform == 'darwin'
 TON_NODE_CONTROL_HOME = os.getenv('TON_NODE_CONTROL_HOME')
+TON_BUILD_REQUIREMENTS = [
+    'git',
+    'make',
+    'cmake',
+    'clang',
+    'pkg-config',
+    'libgflags-dev',
+    'zlib1g-dev',
+    'libssl-dev',
+    'libreadline-dev',
+    'libmicrohttpd-dev',
+    'libgsl-dev',
+    'libmicrohttpd-dev',
+    'python3',
+    'python3-dev',
+    'python3-pip',
+]
 
 # Script utils
 FOREGROUND_COLORS: t.Dict[COLOR, Integer] = dict(
@@ -275,19 +294,44 @@ class Cursor:
         return self
 
 
-class VirtualEnvironment:
+class Builder:
     def __init__(self, path: pathlib.Path) -> None:
         self._path: pathlib.Path = path
         self._binaries_path: pathlib.Path = self._path.joinpath('bin')
-        self._python: String = str(self._path.joinpath(self._binaries_path, 'python'))
 
     @property
     def path(self) -> pathlib.Path:
         return self._path
-    
+
     @property
     def binaries_path(self) -> pathlib.Path:
         return self._binaries_path
+
+    @classmethod
+    def make(cls, target: pathlib.Path) -> Builder:
+        raise NotImplementedError
+
+    @staticmethod
+    def run(*args: t.Any, **kwargs: t.Dict[String, t.Any]) -> subprocess.CompletedProcess:
+        process = subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            **kwargs,
+        )
+        if process.returncode != 0:
+            raise TonNodeControlInstallationError(
+                log=process.stdout.decode(),
+                return_code=process.returncode,
+            )
+        return process
+
+
+class VirtualEnvironment(Builder):
+    def __init__(self, path: pathlib.Path) -> None:
+        super().__init__(path)
+        self._python: String = str(self._path.joinpath(self._binaries_path, 'python'))
+        self._git: String = str(self._path.joinpath(self._binaries_path, 'git'))
     
     @classmethod
     def make(cls, target: pathlib.Path) -> VirtualEnvironment:
@@ -328,21 +372,6 @@ class VirtualEnvironment:
 
         env.pip('install', '--disable-pip-version-check', '--upgrade', 'pip')
         return env
-    
-    @staticmethod
-    def run(*args, **kwargs) -> subprocess.CompletedProcess:
-        process = subprocess.run(
-            args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            **kwargs,
-        )
-        if process.returncode != 0:
-            raise TonNodeControlInstallationError(
-                log=process.stdout.decode(),
-                return_code=process.returncode,
-            )
-        return process
 
     def python(self, *args, **kwargs) -> subprocess.CompletedProcess:
         return self.run(self._python, *args, **kwargs)
@@ -351,9 +380,40 @@ class VirtualEnvironment:
         return self.python('-m', 'pip', *args, **kwargs)
 
 
+class Compiler(Builder):
+    def __init__(self, path: pathlib.Path) -> None:
+        super().__init__(path)
+        self._path: pathlib.Path = path
+
+    @classmethod
+    def make(cls, target: pathlib.Path) -> Compiler:
+        compiler: Compiler = cls(target)
+        for requirement in TON_BUILD_REQUIREMENTS:
+            path: t.Optional[String] = shutil.which(requirement)
+            if path is None:
+                compiler.apt_get(requirement)
+        os.environ.setdefault('CC', shutil.which('clang'))
+        os.environ.setdefault('CXX', shutil.which('clang++'))
+        os.environ.setdefault('CCACHE_DISABLE', '1')
+        return compiler
+    
+    def apt_get(self, *args, **kwargs) -> subprocess.CompletedProcess:
+        return self.run('sudo', 'apt-get', 'install', '-y', *args, **kwargs)
+    
+    def cmake(self, *args, **kwargs) -> subprocess.CompletedProcess:
+        return self.run(
+            'cmake', '-DCMAKE_BUILD_TYPE=Release', '-Wno-dev', '-B', self.path,
+            *args, **kwargs,
+        )
+
+    def make_build(self, *args, **kwargs) -> subprocess.CompletedProcess:
+        return self.run('make', '-j', os.cpu_count(), *args, **kwargs)
+
+
 class Installer:
     MEDATA_URL: String = 'https://pypi.org/pypi/ton-node-control/json'
     TON_MEDATA_URL: String = 'https://api.github.com/repos/ton-blockchain/ton/commits'
+    TON_SOURCES_URL: String = 'https://api.github.com/repos/ton-blockchain/ton/tarball/{version}'
     VERSION_REGEX = re.compile(
         r"v?(\d+)(?:\.(\d+))?(?:\.(\d+))?(?:\.(\d+))?"
         "("
@@ -371,34 +431,40 @@ class Installer:
         accept_all: Bool = False,
         git: t.Optional[String] = None,
         version: t.Optional[String] = None,
+        ton_version: t.Optional[String] = None,
         preview: Bool = False,
     ) -> None:
         self._force: Bool = force
         self._accept_all: Bool = accept_all
         self._git: t.Optional[String] = git
         self._version: t.Optional[String] = version
-        # FIXME: Add variant to install specified commit of "ton-blockchain"
-        self._ton_version: t.Optional[String] = None
+        self._ton_version: t.Optional[String] = ton_version
         self._preview: Bool = preview
 
         self._cursor = Cursor()
         self._module_dir: pathlib.Path = module_directory()
         self._binaries_dir: pathlib.Path = binary_directory()
-        self._ton_binaries_dir: pathlib.Path = binary_directory()
+        self._ton_binaries_dir: pathlib.Path = ton_binary_directory()
         self._meta_data: t.Dict[t.Any, t.Any] = {}
         self._ton_meta_data: t.Dict[t.Any, t.Any] = {}
 
     @property
     def binaries_dir(self) -> pathlib.Path:
         if self._binaries_dir is None:
-            self._binaries_dir = binary_directory()
+            self._binaries_dir: pathlib.Path = binary_directory()
         return self._binaries_dir
 
     @property
-    def module_dir(self):
+    def module_dir(self) -> pathlib.Path:
         if self._module_dir is None:
-            self._module_dir = module_directory()
+            self._module_dir: pathlib.Path = module_directory()
         return self._module_dir
+
+    @property
+    def ton_binaries_dir(self) -> pathlib.Path:
+        if self._ton_binaries_dir is None:
+            self._ton_binaries_dir: pathlib.Path = ton_binary_directory()
+        return self._ton_binaries_dir
 
     @property
     def version_file(self) -> pathlib.Path:
@@ -407,6 +473,12 @@ class Installer:
     @property
     def ton_version_file(self) -> pathlib.Path:
         return self._ton_binaries_dir.joinpath('VERSION')
+
+    @property
+    def ton_version(self) -> t.Optional[String]:
+        if self._ton_version is None:
+            return None
+        return self._ton_version[:6]
 
     def _get(self, url: String) -> Bytes:
         request = urllib.request.Request(url, headers={'User-Agent': 'ton-node-control'})
@@ -439,7 +511,8 @@ class Installer:
     def ensure_directories(self) -> None:
         self.module_dir.mkdir(parents=True, exist_ok=True)
         self.binaries_dir.mkdir(parents=True, exist_ok=True)
-    
+        self.ton_binaries_dir.mkdir(parents=True, exist_ok=True)
+
     def get_package_meta_data(self) -> t.Tuple[t.Optional[String], t.Optional[String]]:
         current_version = None
         if self.version_file.exists():
@@ -533,27 +606,11 @@ class Installer:
             version = self._ton_version
             if version is None:
                 version = commits.pop()
-            # version: String = self._ton_version
-            # latest_version = next(
-            #     _version for _version in releases
-            #     if datetime.fromisoformat(
-            #         _version['commit']['author']['date'].strip('Z'),
-            #     ) > date
-            # )
-            # print(latest_version)
-            # return None, latest_version
-            # else:
-            #     latest_version = releases.pop()['sha']
-            #     if current_version == version and self._force is False:
-            #         self._write(
-            #             f'The latest version ({colorize("bold", version)}) is already installed.',
-            #         )
-            #     return None, current_version
+            raise NotImplementedError('FIXME')
         except Exception as err:
             import traceback
             traceback.print_exc()
             print(err)
-
 
     @contextlib.contextmanager
     def make_environment(self, version: String) -> VirtualEnvironment:
@@ -587,7 +644,6 @@ class Installer:
                     version, 'Restoring previously saved environment.'
                 )
                 shutil.move(env_path_saved, env_path)
-            print('findme', err)
             raise err
         else:
             if env_path_saved.exists():
@@ -608,6 +664,42 @@ class Installer:
             symlink.unlink(missing_ok=True)
             self.make_binary(version, env)
 
+    @contextlib.contextmanager
+    def make_compiler(self, version: String) -> Compiler:
+        sources_backup_path: pathlib.Path = self.ton_binaries_dir.with_suffix('.backup')
+        if self.ton_binaries_dir.exists():
+            self._install_comment(
+                version,
+                'Saving existing build',
+            )
+            if sources_backup_path.exists():
+                shutil.rmtree(sources_backup_path)
+            shutil.move(self.ton_binaries_dir, sources_backup_path)
+        try:
+            self._install_comment(
+                self.ton_version,
+                'Preparing "ton-blockchain" sources to compilation',
+            )
+            yield Compiler.make(self.ton_binaries_dir)
+        except Exception as err:
+            if self.ton_binaries_dir.exists():
+                self._install_comment(
+                    version,
+                    'An error occurred. Removing build directories',
+                )
+                shutil.rmtree(self.ton_binaries_dir)
+
+            if sources_backup_path.exists():
+                self._install_comment(
+                    version,
+                    'Restoring previously saved build',
+                )
+                shutil.move(sources_backup_path, self.ton_binaries_dir)
+            raise err
+        else:
+            if sources_backup_path.exists():
+                shutil.rmtree(sources_backup_path, ignore_errors=True)
+
     def install_ton_node_control(self, version: String, env: VirtualEnvironment) -> None:
         self._install_comment(version, 'Installing "ton-node-control"')
         if self._git is not None:
@@ -616,16 +708,50 @@ class Installer:
             specification = f'ton-node-control=={version}'
         env.pip('install', specification)
 
-    def install_ton(self) -> None:
-        self._overwrite(colorize(
-            'info',
-            f'Retrieving "ton-blockchain" meta-data',
-        ))
-        self._ton_meta_data = dict(commits=json.loads(self._get(self.TON_MEDATA_URL)))
-        # if self._ton_version is None:
-        #     version
-        import time
+    def install_ton(self, version: String) -> None:
+        truncated_version = version[:6]
+        self._install_comment(
+            truncated_version,
+            colorize(
+                'info',
+                f'Cloning "ton-blockchain" sources',
+            ),
+        )
+        with self.make_compiler(truncated_version) as compiler:
+            self.compile_ton_sources(truncated_version, compiler)
         time.sleep(5)
+
+    def compile_ton_sources(self, version: String, compiler: Compiler) -> None:
+        data: Bytes = self._get(self.TON_SOURCES_URL.format(version=version))
+        with tempfile.TemporaryDirectory(prefix='ton-blockchain-installer') as temp_dir:
+            tarball_path = pathlib.Path(temp_dir)
+            archive_path: pathlib.Path = tarball_path.joinpath('sources.tar.gz')
+            with open(archive_path, 'w+b') as file:
+                file.write(data)
+            self._install_comment(
+                version,
+                colorize(
+                    'info',
+                    f'Unpacking "ton-blockchain" sources to compile',
+                ),
+            )
+            tarfile.open(file.name).extractall(tarball_path)
+            os.remove(archive_path)
+            sources_dir: String = os.listdir(tarball_path).pop()
+            sources_path: pathlib.Path = tarball_path.joinpath(sources_dir)
+            self._compile_ton(version, compiler, sources_path)
+
+    def _compile_ton(
+        self,
+        version: String,
+        compiler: Compiler,
+        sources_path: pathlib.Path,
+    ) -> None:
+        self._install_comment(
+            version,
+            'Running cmake',
+        )
+        compiler.cmake('-S', sources_path)
 
     def _install_fift(self) -> None:
         pass
@@ -653,8 +779,15 @@ class Installer:
             ),
         )
         self.ensure_directories()
+        # try:
+        #     self.install(version)
+        # except subprocess.CalledProcessError as err:
+        #     raise TonNodeControlInstallationError(
+        #         log=err.output.decode(),
+        #         return_code=err.returncode,
+        #     ) from err
         try:
-            self.install(version)
+            self.install_ton(ton_version)
         except subprocess.CalledProcessError as err:
             raise TonNodeControlInstallationError(
                 log=err.output.decode(),
@@ -674,19 +807,18 @@ class Installer:
         """
         Installs "ton-node-control" in $TON_NODE_CONTROL_HOME.
         """
-        write_styled_stdout(
-            'comment',
-            f'Installing '
-            f'{colorize("info", "ton-node-control")} '
-            f'({colorize("info", version)})',
+        self._write(
+            colorize(
+                'comment',
+                f'Installing {colorize("info", "ton-node-control")} '
+                f'({colorize("info", version)})',
+            ),
         )
         with self.make_environment(version) as env:
             self.install_ton_node_control(version, env)
-            self.install_ton()
             self.make_binary(version, env)
             self.version_file.write_text(version)
             self._install_comment(version, 'Done')
-            
             return 0
 
     def uninstall(self) -> Integer:
@@ -719,7 +851,7 @@ def main() -> Integer:
             colorize('bold', 'Windows system is not supported yet.'),
         )
         return 1
-
+    #
     # if os.getuid() != 0:
     #     # FIXME: Improve language and information
     #     write_styled_stdout('error', 'You have to run this script as sudo to proceed.')
@@ -728,7 +860,8 @@ def main() -> Integer:
     parser = argparse.ArgumentParser(
         description='Installs the latest (or given) version of "ton-node-control".',
     )
-    parser.add_argument('--version', dest='version', help='install named version.')
+    parser.add_argument('--version', dest='version', help='install specific version.')
+    parser.add_argument('--ton-version', dest='ton_version', help='install specific version.')
     parser.add_argument(
         '--git',
         action='store',
@@ -764,6 +897,7 @@ def main() -> Integer:
         force=args.force,
         git=args.git,
         version=args.version,
+        ton_version=args.ton_version,
     )
 
     if args.uninstall is True:
@@ -771,20 +905,20 @@ def main() -> Integer:
     try:
         return installer.run()
     except TonNodeControlInstallationError as err:
-        write_styled_stdout('error', '"ton-node-control" installation failed!')
+        write_styled_stdout('error', 'Installation of "ton-node-control" is failed!')
         
         if err.log is not None:
             import traceback
             
             _, path = tempfile.mkstemp(
                 suffix='.log',
-                prefix='tnc-installer-error',
+                prefix='tnc-installer-error-',
                 dir=str(pathlib.Path.cwd()),
                 text=True,
             )
-            write_styled_stdout('error', f'See {path} for error logs.')
+            write_styled_stdout('error', f'See "{path}" for error logs.')
             pathlib.Path(path).write_text(
-                f'{err.log}\n',
+                f'{err.log}\n'
                 f'Traceback:\n\n{str().join(traceback.format_tb(err.__traceback__))}',
             )
         return err.return_code
--- tests/src/cli/test_system_functions.py
+++
@@ -1,8 +0,0 @@
-from ton_node_control.cli.utils.system import get_cpu_count
-
-
-class TestSystem:
-    def test_get_cpu_count(self) -> None:
-        cpu_count: int = get_cpu_count()
-        assert cpu_count is not None
-        assert isinstance(cpu_count, int)
