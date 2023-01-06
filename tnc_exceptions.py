94406b3cff3d0a4c55c94beab54f4135a75079ab
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2023-01-06 20:43:46 +0300

    Added cursor and exceptions to separated file

---
+++ tnc_exceptions.py
@@ -0,0 +1,10 @@
+import typing as t
+
+from tnc_typing import Integer, String
+
+
+class TonNodeControlInstallationError(RuntimeError):
+    def __init__(self, return_code: Integer = 0, log: t.Optional[String] = None) -> None:
+        super().__init__()
+        self.return_code: Integer = return_code
+        self.log: t.Optional[String] = log
