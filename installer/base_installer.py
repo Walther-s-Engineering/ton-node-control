43a4e1234ee679ade014c53efb31fe2dee165c1f
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2023-01-09 02:06:02 +0300

    @WIP

--- installer/base_installer.py
+++ installer/base_installer.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
 
+import contextlib
+import functools
+import json
 import typing as t
 
 import re
 import sys
 
-from installer.typing import String, Integer
-from installer.styling import is_decorated
+from pathlib import Path
+from urllib.request import Request, urlopen
+
+from installer.styling import colorize, is_decorated
+from installer.sources import get_binaries_directory, get_module_directory, get_ton_binaries_directory
+from installer.typing import Bytes, String, Integer
 
 
 class Cursor:
@@ -118,7 +125,40 @@ class Installer:
     ) -> None:
         self.version: t.Optional[String] = version
         self.ton_version: t.Optional[String] = ton_version
+        
         self.cursor = Cursor()
+        
+        self._module_directory: Path = get_module_directory()
+        self._binaries_directory: Path = get_binaries_directory()
+        self._ton_binaries_directory: Path = get_ton_binaries_directory()
+        
+        self._meda_data: t.Dict[t.Any, t.Any] = {}
+
+    @property
+    def module_directory(self) -> Path:
+        return self._module_directory
+
+    @property
+    def binaries_directory(self) -> Path:
+        return self._binaries_directory
+    
+    @property
+    def ton_binaries_directory(self) -> Path:
+        return self._ton_binaries_directory
+
+    @property
+    def version_file(self) -> Path:
+        return self.module_directory.joinpath('VERSION')
+    
+    @property
+    def ton_version_file(self) -> Path:
+        return self.ton_binaries_directory.joinpath('VERSION')
+
+    @staticmethod
+    def _get(url: String) -> Bytes:
+        request = Request(url, headers={'User-Agent': 'ton-node-control'})
+        with contextlib.closing(urlopen(request)) as response:
+            return response.read()
 
     @staticmethod
     def _write(line: String) -> None:
@@ -127,6 +167,78 @@ class Installer:
     def _overwrite(self, line: String) -> None:
         if not is_decorated():
             return self._write(line)
-    
+        self.cursor.move_up()
+        self.cursor.clear_line()
+        self._write(line)
+
+    def _install_comment(self, version: String, message: String) -> None:
+        self._overwrite(
+            'Installing {} ({}): {}'.format(
+                colorize('info', '"ton-node-control"'),
+                colorize('bold', version),
+                colorize('comment', message),
+            ),
+        )
+
+    def ensure_directories(self) -> None:
+        self.module_directory.mkdir(parents=True, exist_ok=True)
+        self.binaries_directory.mkdir(parents=True, exist_ok=True)
+        self.ton_binaries_directory.mkdir(parents=True, exist_ok=True)
+
+    def get_package_meta_data(self) -> t.Tuple[t.Optional[String], t.Optional[String]]:
+        current_version = None
+        if self.version_file.exists():
+            current_version = self.version_file.read_text().strip()
+        self._write(
+            colorize(
+                'info',
+                f'Retrieving "ton-node-control" meta-data',
+            ),
+        )
+        self._meda_data = json.loads(self._get(self.MEDATA_URL).decode())
+        
+        def _compare_versions(current: String, lookup: String) -> Integer:
+            version_current: re.Match = self.VERSION_REGEX.match(current)
+            version_lookup: re.Match = self.VERSION_REGEX.match(lookup)
+            version_current: t.Tuple[Integer] = tuple(
+                int(p)
+                for p in version_current.groups()[:3] + (version_current.group(5),)
+            )
+            version_lookup: t.Tuple[Integer] = tuple(
+                int(p)
+                for p in version_lookup.groups()[:3] + (version_lookup.group(5),)
+            )
+            if version_current < version_lookup:
+                return -1
+            if version_current > version_lookup:
+                return 1
+            return 0
+        
+        self._write('')
+        releases = sorted(
+            self._meda_data['releases'].keys(),
+            key=functools.cmp_to_key(_compare_versions),
+        )
+        if self.version is not None and self.version not in releases:
+            message = colorize('error', f'Version "{self.version} does not exists."')
+            self._write(message)
+            raise ValueError(message)
+        
+        version = self.version
+        if version is None:
+            for release in reversed(releases):
+                match = self.VERSION_REGEX.match(release)
+                if match.group(5):  # FIXME: and self.allow_pre_releases() is False:
+                    continue
+                version = release
+                break
+        if current_version == version:  # FIXME: and self._force is False:
+            self._write(
+                f'The latest version ({colorize("bold", version)}) is already installed.',
+            )
+            return None, current_version
+        return version, current_version
+
     def install(self) -> Integer:
+        self._write('Installing')
         return 1
--- installer/sources.py
+++ installer/sources.py
@@ -1,17 +1,53 @@
+import site
+import sys
 import typing as t
 
+import os
 
-def get_ton_node_control_home():
-    pass
+from pathlib import Path
+
+from installer.typing import String
+
+TON_NODE_CONTROL_HOME: Path = Path(os.getenv('TON_NODE_CONTROL_HOME'))
+
+SOURCES_PATH: t.Dict[String, Path] = {
+    'linux': Path('/usr/src'),
+    # TODO: Improve this for MacOS
+    'darwin': Path('/usr/src'),
+}
+
+BINARIES_PATH: t.Dict[String, Path] = {
+    'linux': Path('/usr/bin'),
+    # TODO: Improve this for MacOS
+    'darwin': Path('/usr/bin'),
+}
+
+MODULE_PATH: t.Dict[String, Path] = {
+    'linux': Path(site.getuserbase()).joinpath('share'),
+    # TODO: Improve this for MacOS
+    'darwin': Path('~/Library/Application Support'),
+}
+
+
+def get_ton_node_control_home(*targets: t.Optional[String]) -> Path:
+    if not targets:
+        return TON_NODE_CONTROL_HOME.expanduser()
+    return Path(TON_NODE_CONTROL_HOME, *targets).expanduser()
 
 
 def get_module_directory():
-    pass
+    if TON_NODE_CONTROL_HOME is not None:
+        return get_ton_node_control_home()
+    return MODULE_PATH[sys.platform].joinpath('ton-node-control')
 
 
 def get_binaries_directory():
-    pass
+    if TON_NODE_CONTROL_HOME is not None:
+        return get_ton_node_control_home('bin')
+    return get_module_directory().joinpath('bin')
 
 
-def ton_binaries_directory():
-    pass
+def get_ton_binaries_directory():
+    if TON_NODE_CONTROL_HOME is not None:
+        return get_ton_node_control_home()
+    return get_module_directory().joinpath('bin')
